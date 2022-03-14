d5740f6c7aa392c7cc5c11a3590f52e90f045ac9
Lewis Crichton <lewistehminerz@outlook.com>
2022-03-14 21:16:54 +0000

    remove scrolling js

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,21 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-
-	import { browser } from '$app/env';
-
-	if (browser) {
-		let barTimeout: NodeJS.Timeout; // uses wrong type but whatever
-
-		document.body.onscroll = () => {
-			if (barTimeout) {
-				clearTimeout(barTimeout); //clear to reset
-			}
-			barTimeout = setTimeout(() => {
-				document.body.classList.remove('scrolling');
-			}, 500); //0.5s delay
-			document.body.classList.add('scrolling');
-		};
-	}
 </script>
 
 <main>
